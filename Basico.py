3e7dc622abc9aacb2e604d623dde7133ee056be6
JP5PRO <jp5200908@gmail.com>
2025-03-08 12:15:17 -0600

    Update Basico.py

--- Basico.py
+++ Basico.py
@@ -1,3 +1,9 @@
+Empezamos el curso, explicando lo más básico y fácil en Python. Para iniciar debemos saber que si queremos mostrar 
+un mensaje por consola, con un simple "print("")" se puede lograr como en el primer ejemplo. Y otra cosa muy importante es 
+que según Python no importa la especificación del dato (int, varchar, double, etc), tu puedes definir un dato con el valor 
+que quieras y Python analizará el tipo de dato que ha escrito. En el segundo ejemplo se muestra como realizo 4 operaciones 
+distintas con un dato int y otro float y muestro los mensajes por consola dando su resultado, para esto en código debes 
+escribir print("tu_mensaje", tu_variable ,) aunque esto se puede hacer de distintas maneras esta es la más básica.
 #________________________________________________________________________
 print("Hola mundo")
 
